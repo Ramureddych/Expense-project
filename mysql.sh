d4dee005faff1b47157087d49d1f516c125e6fe6
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-24 23:18:26 +0530

    mysql-installing-through-shell

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-SCRIPT_NAME=$(echo &0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo &0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
