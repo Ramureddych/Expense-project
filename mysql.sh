3948c6399b075be1a2f280cd81d6d1ea371dc99e
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-24 23:36:45 +0530

    mysql-installing-through-shell

--- mysql.sh
+++ mysql.sh
@@ -5,6 +5,16 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is failing..."
+        exit 1
+    else
+        echo "$2 is success"
+    fi        
+}
+
 
 if [ $USERID -ne 0 ]
 then
@@ -18,15 +28,5 @@ fi
 dnf install mysql-server -y  &>>$LOGFILE
 validate $? "installing mysql-server"
 
-validate(){
-    if [ $1 -ne 0 ]
-    then 
-        echo "$2 is failing..."
-        exit 1
-    else
-        echo "$2 is success"
-    fi        
 
 
-
-}
