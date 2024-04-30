1ec796ad759d7ff386dca17f8c21f23ea3b0553e
chilaka ramureddy ramu <ramureddy0457@gmail.com>
2024-04-30 19:24:17 +0530

    mysql-installing-through-shell

--- mysql.sh
+++ mysql.sh
@@ -1,55 +1,55 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo "Please enter DB password:"
+read -s mysql_root_password
 
-
-validate(){
-
-    if [ $1 -ne 0 ]
-    then 
-        echo "$2 is failing..."
+VALIDATE(){
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo "$2 is success"
-    fi        
+        echo -e "$2...$G SUCCESS $N"
+    fi
 }
-
-
-
-
 
 if [ $USERID -ne 0 ]
 then
-    echo "you are not a super user"
-    exit 1
-else 
-    echo "you are a super user, please proceed"
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "You are super user."
 fi
 
 
-dnf install mysql-server -y  &>>$LOGFILE
-validate $? "installing mysql-server"
-
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "Installing MySQL Server"
 
 systemctl enable mysqld &>>$LOGFILE
-validate $? "enabled mysqld service"
-
+VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>>$LOGFILE
-validate $? "started mysqld service"
+VALIDATE $? "Starting MySQL Server"
 
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# validate $? "sets root password dor mysqlservice"
+# VALIDATE $? "Setting up root password"
 
-#this below code is for code to be idempotent in nature
-
-
-mysql -h db.chilaka.fun -u root -pExpenseApp@1
-
-
+#Below code will be useful for idempotent nature
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "MySQL Root password Setup"
+else
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
+fi
 
 
